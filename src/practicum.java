12cdd56aa9235a288d622afc6924c0e4836d4c49
Jochem de Wit <jochemdewit49@gmail.com>
2018-09-28 14:22:04 +0200

    Initial commit

---
+++ src/practicum.java
@@ -0,0 +1,23 @@
+public class practicum
+{
+
+    public static void main(String[] args)
+    {
+
+        boolean pindakaas = true;
+
+        String stukkieTekst = "Pingu ";
+        String andereTekst = "is great anime";
+        String samenTekst = stukkieTekst + andereTekst;
+
+        int getalA = 123;
+        float getalB = 5.5f;
+        double getalC = 10.3d;
+
+        System.out.println(pindakaas);
+        System.out.println(samenTekst);
+        System.out.println(getalA + getalB + getalC);
+
+    }
+
+}
